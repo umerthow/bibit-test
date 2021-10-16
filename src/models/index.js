33b69cc8f5340d3e728d6a561b4178f81bd46637
ahmad.umar <ahmad.umar@bizzy.co.id>
2021-10-16 09:52:26 +0700

    update model modul

--- src/models/index.js
+++ src/models/index.js
@@ -1,20 +1,5 @@
-const Sequelize = require('sequelize');
-const db = require('./../configs/db');
+const Logs = require('./logs_models')
 
-const LogModel = db.define('logs', {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  url: { type: Sequelize.STRING },
-  params: { type: Sequelize.STRING },
-  method: { type: Sequelize.STRING }
-},
-{
-  createdAt: 'created_at',
-  updatedAt: 'updated_at',
-  freezeTableName: true
-});
-
-module.exports = LogModel;
+module.exports = {
+  Logs
+}
