c70d2709dfe3ed38904b6fe33c9e6c15d840fe24
ahmad.umar <ahmad.umar@bizzy.co.id>
2021-10-17 11:14:28 +0700

    update v1

--- src/controllers/movies_controller.js
+++ src/controllers/movies_controller.js
@@ -1,18 +1,23 @@
 const response = require('../utils');
-const OMDB_API = require('../library/omdbAPI');
-const omdbKey = 'faf7e5bb';
-const omdbAPi = new OMDB_API(omdbKey);
-
+const movieService = require('../services/movie_service')
 const MovieController = {
   getData: async (req, res, next) => {
     try {
-      const { query: params } = req;
-      const result = await omdbAPi.searchMovies(params.movie_name, params.page);
-      if (result && result.Response !== 'False') {
-        const { Search } = result;
-        return response.success(res, Search);
+      const { query } = req;
+      
+      if (!query.movie_name) {
+        return response.failed(res, 'Please provide movie_name! eq:`batman` ');
       }
-      return response.failed(res, result.Error);
+
+      const params = {
+        movie_name: query.movie_name,
+        page: query.page || 1
+      }
+
+      const data = await movieService.search(params)
+
+      return response.success(res, data);
+
     } catch (error) {
       next(error);
     }
---
+++ src/services/movie_service.js
@@ -0,0 +1,18 @@
+const OMDB_API = require('../library/omdbAPI');
+const omdbKey = 'faf7e5bb';
+const omdbAPi = new OMDB_API(omdbKey);
+
+const MovieService = {
+  search: async (params) => {
+    const result = await omdbAPi.searchMovies(params.movie_name, params.page);
+    if (result && result.Response !== 'False') {
+      const { Search: data } = result;
+      return data
+    } else {
+     throw Error(result.Error)
+    }
+  }
+}
+
+
+module.exports = MovieService;
